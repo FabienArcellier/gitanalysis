5cd71ce5e77d3930588aaf884c56135076922140
Fabien Arcellier <farcellier@octo.com>
2019-02-28 18:04:23 +0100

    fix linter error

--- gitanalysis/infrastructure/git_shell.py
+++ gitanalysis/infrastructure/git_shell.py
@@ -8,9 +8,11 @@ from gitanalysis.domain.git import Git
 class GitShell(Git):
     def log(self):
         if sys.version_info >= (3, 0):
-            output = subprocess.check_output("git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN'", shell=True,
+            output = subprocess.check_output("git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN'",
+                                             shell=True,
                                              encoding='utf8')
         else:
-            output = subprocess.check_output("git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN'", shell=True)
+            output = subprocess.check_output("git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN'",
+                                             shell=True)
             output = output.decode('utf-8')
         return output
